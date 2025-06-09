7e61e989c5a03011ae0a09adeaee030f845d8bf7
Johannes Goller <j@jogojapan.xyz>
2025-06-09 13:07:54 +0200

    Moving messages to debug mode. Cleaning up.

--- content.js
+++ content.js
@@ -1,29 +1,15 @@
-console.log('Content script loaded successfully');
+console.debug('Content script loaded successfully');
 
 function requestFullScreen(element) {
     if (element.requestFullscreen) {
         element.requestFullscreen();
     } else if (element.webkitRequestFullscreen) {
         element.webkitRequestFullscreen();
-    } // Add other prefixes as needed
+    }
 }
 
 function initScript() {
-  // Your existing logic to find the image element
   let imageElement;
-  const observer = new MutationObserver(() => {
-    console.log('Mutation observed')
-    imageElement = document.querySelector('img[alt="Main Content"]');
-    if (imageElement) {
-      console.log('Image element found and ready.');
-    }
-  });
-
-  const targetNode = document.querySelector('.mantine-Carousel-viewport');
-  if (targetNode) {
-    observer.observe(targetNode, { childList: true, subtree: true });
-  }
-
   chrome.runtime.onMessage.addListener((message) => {
     if (message.action === "requestFullScreen") {
       const fullUrl = message.imageUrl;  // Full URL from context menu
@@ -37,16 +23,15 @@ function initScript() {
       }
 
       if (targetImage) {
-        console.log('Target image found and requesting full-screen.');
+        console.debug('Target image found and requesting full-screen.');
         requestFullScreen(targetImage);
       } else {
-        console.error('Target image still not found. URL used:', fullUrl);
+        console.debug('Target image still not found. URL used:', fullUrl);
       }
     }
   });
 }
 
-// Run the script as before
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', initScript);
 } else {
